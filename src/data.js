9faca582932516c2561ada0bf15970c1f4034c3e
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-04-14 21:54:39 +0600

    mapped data from js object

---
+++ src/data.js
@@ -0,0 +1,65 @@
+const Info = {
+  profile: {
+    name: 'Ehsan Khan',
+    title: 'Web Developer',
+    about:
+      'A passionate developer, competitive programer and quick learner. Typical introvert. Love to code and learn new things.',
+  },
+  contact: {
+    email: 'ehsan18t@gmail.com',
+    phone: '+8801641723411',
+    github: 'ehsan18t',
+    linkedin: 'ehsan18t',
+  },
+  education: [
+    {
+      institute: 'United International University',
+      degree: 'B.Sc. in Computer Science and Engineering',
+      year: '2020 - Present',
+    },
+  ],
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Tailwind CSS',
+    'React',
+    'Svelte',
+    'C/C++',
+    'Java',
+    'Python',
+    'Django',
+    'SQL',
+    'PHP',
+    'C#',
+    'Git',
+    'GitHub',
+    'Vim',
+    'Bash',
+    'Batch',
+    'Markdown',
+    'Jira',
+    'Scrum',
+  ],
+  experience: [
+    {
+      company: 'MagicX Mod',
+      position: 'OS Modder',
+      duration: '2018 - Present',
+    },
+    {
+      company: 'MIUI Infinity',
+      position: 'Android ROM Developer',
+      duration: '2018 - 2019',
+    },
+  ],
+  projects: [
+    {
+      name: 'Win-10-Ultimate-System-Tweaks',
+      link: 'https://github.com/ehsan18t/Win10-Ultimate-System-Tweaks',
+      description:
+        'A script/tool to make windows 10 better by adding useful features & tools, disabling useless services, turning off logging and sending data to third-party and removing garbage.',
+      tech: ['Batch', 'Powershell', 'VBS', 'Registry'],
+    },
+  ],
+}
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,9 +5,10 @@ import Email from '../components/icons/Email'
 import Github from '../components/icons/Github'
 import Telephone from '../components/icons/Telephone'
 import useDocumentTitle from '../assets/js/useDocumentTitle'
+import Info from '../data'
 
 const Resume = () => {
-  useDocumentTitle("Ehsan Khan's Profile")
+  useDocumentTitle(Info.profile.name + "'s Profile")
 
   return (
     <div className="container">
@@ -17,12 +18,9 @@ const Resume = () => {
         </div>
 
         <div className="profile">
-          <h1>Ehsan Khan</h1>
-          <h2>Software Developer</h2>
-          <p>
-            A passionate developer, competitive programer, quick learner.
-            Typical introvert. Love to code and learn new things.
-          </p>
+          <h1>{Info.profile.name}</h1>
+          <h2>{Info.profile.title}</h2>
+          <p>{Info.profile.about} </p>
           <div className="hr">
             <br />
             <br />
@@ -33,82 +31,59 @@ const Resume = () => {
         <div className="left-side">
           <div className="contact">
             <h2>Contact</h2>
-            <a href="mailto:ehsan18t@gmail.com">
-              <Email className="svg-icon" /> ehsan18t@gmail.com
+            <a href={'mailto:' + Info.contact.email}>
+              <Email className="svg-icon" /> {Info.contact.email}
             </a>
-            <a href="tel:01700000000">
-              <Telephone className="svg-icon" /> +8801641723411
+            <a href={'tel:' + Info.contact.phone}>
+              <Telephone className="svg-icon" /> {Info.contact.phone}
             </a>
-            <a href="https://github.com/ehsan18t">
-              <Github className="svg-icon" /> @ehsan18t
+            <a href={'https://github.com/' + Info.contact.github}>
+              <Github className="svg-icon" /> @{Info.contact.github}
             </a>
-            <a href="https://www.linkedin.com/in/ehsan18t/">
-              <Linkedin className="svg-icon" /> @ehsan18t
+            <a href={'https://www.linkedin.com/in/' + Info.contact.linkedin}>
+              <Linkedin className="svg-icon" /> @{Info.contact.linkedin}
             </a>
           </div>
 
           <div className="education">
             <h2>Education</h2>
-            <div className="school">
-              <h3>United International University</h3>
-              <h4>Computer Science Engineering</h4>
-              <p>2020-Present</p>
-            </div>
+            {Info.education.map((edu) => (
+              <div className="institute">
+                <h3>{edu.institute}</h3>
+                <h4>{edu.degree}</h4>
+                <p>{edu.duration}</p>
+              </div>
+            ))}
           </div>
         </div>
         <div className="right-side">
           <div className="skills">
             <h2>Skills</h2>
             <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>TailwindCSS</li>
-              <li>Svelte</li>
-              <li>React</li>
-              <li>Django</li>
-              <li>SQL</li>
-              <li>Git</li>
-              <li>GitHub</li>
-              <li>C/C++</li>
-              <li>Java</li>
-              <li>Python</li>
-              <li>C#</li>
-              <li>PHP</li>
-              <li>Vim</li>
-              <li>Bash</li>
-              <li>Batch</li>
-              <li>Markdown</li>
-              <li>Jira</li>
-              <li>Scrum</li>
+              {Info.skills.map((skill) => (
+                <li>{skill}</li>
+              ))}
             </ul>
           </div>
           <div className="projects">
             <h2>Projects</h2>
-            <div className="project">
-              <h3>Win-10-Ultimate-System-Tweaks</h3>
-              <p>
-                A script/tool to make windows 10 better by adding useful
-                features & tools, disabling useless services, turning off
-                logging and sending data to third-party and removing garbage.
-              </p>
-              <a href="https://github.com/ehsan18t/Win10-Ultimate-System-Tweaks">
-                View on GitHub
-              </a>
-            </div>
+            {Info.projects.map((project) => (
+              <div className="project">
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+                <a href={project.link}>View on GitHub</a>
+              </div>
+            ))}
           </div>
           <div className="experience">
             <h2>Experience</h2>
-            <div className="job">
-              <h3>OS Modder</h3>
-              <h4>MagicX Mod</h4>
-              <p>2018-Present</p>
-            </div>
-            <div className="job">
-              <h3>Android ROM Developer</h3>
-              <h4>InfinityOS</h4>
-              <p>2018-2019</p>
-            </div>
+            {Info.experience.map((exp) => (
+              <div className="job">
+                <h3>{exp.position}</h3>
+                <h4>{exp.company}</h4>
+                <p>{exp.duration}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
