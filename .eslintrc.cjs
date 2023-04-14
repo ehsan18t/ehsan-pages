8b6b609c9239ec36d855a53455f70731aa6dbfc7
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-04-14 21:18:13 +0600

    eslint config for @apply, @tailwindcss support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,5 +15,20 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', 'prettier'],
-  rules: {},
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+  },
 }
