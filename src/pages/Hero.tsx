f5cf338e92e3b5b2b511e76333f7b06bb949fca3
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-09-19 02:16:04 +0600

    relayout hero section

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -10,17 +10,19 @@ const Hero = () => {
     "w-10 h-10 p-2 ring-2 rounded-full ring-primary cursor-pointer hover:ring-4 hover:text-primary transition duration-300 ease-in-out";
   return (
     <div className="w-full h-full">
-      <div className="w-full h-5/6 flex justify-between items-center px-10">
-        <Me />
-        <StarsCanvas />
+      <div className="flex justify-between items-center px-10">
+        <div className="flex flex-col justify-between">
+          <Introduction />
+          <div className="px-24 flex gap-4 justify-center md:justify-start">
+            <AiFillGithub className={socialIconClass} />
+            <BiLogoTelegram className={socialIconClass} />
+            <BsDiscord className={socialIconClass} />
+            <AiFillLinkedin className={socialIconClass} />
+          </div>
+        </div>
         <TextSphere className="hidden md:block" />
       </div>
-      <div className="px-24 flex gap-4 justify-center md:justify-start">
-        <AiFillGithub className={socialIconClass} />
-        <BiLogoTelegram className={socialIconClass} />
-        <BsDiscord className={socialIconClass} />
-        <AiFillLinkedin className={socialIconClass} />
-      </div>
+      <StarsCanvas />
     </div>
   );
 };
