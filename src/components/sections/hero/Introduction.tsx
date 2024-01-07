f0797083b465db58e897c0d1c5049a14318ce5b9
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-01-07 22:08:39 +0600

    adjusted text sizes for 720p mobile screen

--- src/components/sections/hero/Introduction.tsx
+++ src/components/sections/hero/Introduction.tsx
@@ -1,9 +1,9 @@
 const Introduction = () => {
   return (
     <div className="h-full flex flex-col gap-2 justify-center">
-      <div className="text-3xl font-semibold">Hi, I am</div>
-      <div className="text-6xl font-bold text-cyan-600">Ehsan Khan</div>
-      <div className="w-full p-1 text-xl text-justify pt-5">
+      <div className="text-2xl sm:text-3xl font-semibold">Hi, I am</div>
+      <div className="text-4xl sm:text-6xl font-bold text-cyan-600">Ehsan Khan</div>
+      <div className="w-full p-1 sm:text-xl text-justify pt-5">
         A passionate software developer, competitive programer and quick learner. Love to
         code and learn new things. I mostly work with Django, React, NextJS, TailWindCSS,
         JavaScript, TypeScript, MySQL and Git. Additionally, I have experience with C++,
