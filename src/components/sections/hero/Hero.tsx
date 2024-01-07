d156757bc7e06bcdb529f16ce2f671e5b4dfdeb3
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-01-07 21:49:43 +0600

    removed unused props

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,9 +1,12 @@
 import { SocialBar } from '@/components';
 import Introduction from './Introduction';
 
-const Hero = () => {
+const Hero = ({ itemRef }: any) => {
   return (
-    <section className="flex flex-col items-center justify-between py-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-gray-900 text-gray-200">
+    <section
+      className="flex flex-col items-center justify-between py-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-gray-900 text-gray-200"
+      ref={itemRef}
+    >
       <Introduction />
       <SocialBar />
     </section>
