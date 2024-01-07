b8840397a355a180d7bb7aed889941ae368c944b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-01-07 21:49:30 +0600

    added dummy pages

---
+++ src/components/sections/skills/Skills.tsx
@@ -0,0 +1,12 @@
+const Skills = ({ itemRef }: any) => {
+  return (
+    <section
+      className="flex flex-col items-center justify-between py-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-gray-900 text-gray-200"
+      ref={itemRef}
+    >
+      Skills
+    </section>
+  );
+};
+
+export default Skills;
