01de93a63552917fd3e2f07085d537756520e0ff
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-10 20:46:14 +0600

    added utils

---
+++ src/motion/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { motion } from 'framer-motion';
+
+export const MotionDiv = motion.div;
+export const MotionSpan = motion.span;
+export const MotionH1 = motion.h1;
+export const MotionH2 = motion.h2;
+export const MotionH3 = motion.h3;
+export const MotionH4 = motion.h4;
+export const MotionH5 = motion.h5;
+export const MotionH6 = motion.h6;
+export const MotionP = motion.p;
+export const MotionA = motion.a;
+export const MotionLi = motion.li;
+export const MotionOl = motion.ol;
+export const MotionUl = motion.ul;
