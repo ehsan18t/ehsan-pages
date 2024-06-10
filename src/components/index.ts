f5c8ae13c02ea839a45cc962eb94789acda3382e
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-10 21:02:54 +0600

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,2 @@
-export { default as SocialBar } from './social/SocialBar';
-export { default as Hero } from './sections/hero/Hero';
-export { default as Skills } from './sections/skills/Skills';
-export { default as Experiences } from './sections/experiences/Experiences';
+export { default as Hero } from "./sections/hero/Hero";
+export { default as SocialBar } from "./social/SocialBar";
