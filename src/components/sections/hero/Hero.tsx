a4bc5d5e9bdd2f4b03b313186d5fb4fdbb828086
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-30 15:30:40 +0600

    added cv download button

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -5,9 +5,11 @@ import Photo from "./Photo";
 const Hero = () => {
   return (
     <section className="h-screen pb-0 md:pb-20 px-6 sm:px-12 md:px-32 lg:px-40 bg-background text-foreground">
-      <div className="h-3/4 md:h-full w-full grid md:grid-cols-3 md:gap-10 items-center justify-self-center justify-items-center">
-        <Introduction />
-        <Photo />
+      <div className="h-4/5">
+        <div className="h-full md:h-full w-full grid md:grid-cols-3 md:gap-10 items-center justify-self-center justify-items-center">
+          <Introduction />
+          <Photo />
+        </div>
       </div>
       <SocialBar />
     </section>
--- src/components/social/SocialBar.tsx
+++ src/components/social/SocialBar.tsx
@@ -4,32 +4,39 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { BiLogoGmail, BiLogoTelegram } from "react-icons/bi";
 import { BsDiscord } from "react-icons/bs";
 
+import Button from "../button/Button";
 import Social from "./Social";
 
 const SocialBar = () => {
   const socials = Info.socials;
   return (
-    <MotionDiv
-      className="flex gap-3 justify-center"
-      initial={{ opacity: 0, y: 40 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Social
-        Icon={AiFillGithub}
-        link={`https://github.com/${socials.github}`}
-      />
-      <Social
-        Icon={AiFillLinkedin}
-        link={`https://linkedin.com/in/${socials.linkedin}`}
-      />
-      <Social Icon={BiLogoGmail} link={`mailto:${socials.gmail}`} />
-      <Social Icon={BiLogoTelegram} link={`https://t.me/${socials.telegram}`} />
-      <Social
-        Icon={BsDiscord}
-        link={`https://discord.com/users/${socials.discord}`}
-      />
-    </MotionDiv>
+    <div className="flex flex-col gap-6 items-center justify-center">
+      <Button className="w-56">Download CV</Button>
+      <MotionDiv
+        className="flex gap-3 justify-center"
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Social
+          Icon={AiFillGithub}
+          link={`https://github.com/${socials.github}`}
+        />
+        <Social
+          Icon={AiFillLinkedin}
+          link={`https://linkedin.com/in/${socials.linkedin}`}
+        />
+        <Social Icon={BiLogoGmail} link={`mailto:${socials.gmail}`} />
+        <Social
+          Icon={BiLogoTelegram}
+          link={`https://t.me/${socials.telegram}`}
+        />
+        <Social
+          Icon={BsDiscord}
+          link={`https://discord.com/users/${socials.discord}`}
+        />
+      </MotionDiv>
+    </div>
   );
 };
 
