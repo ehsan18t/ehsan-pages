4ad9d7820e7822d664b31ecbf7d06d3016c84915
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-30 12:23:36 +0600

    Update Hero.tsx

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -3,7 +3,7 @@ import Introduction from "./Introduction";
 
 const Hero = () => {
   return (
-    <section className="h-screen flex flex-col items-center justify-between py-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-background text-foreground">
+    <section className="h-screen flex flex-col items-center justify-between pt-0 py-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-background text-foreground">
       <Introduction />
       <SocialBar />
     </section>
