2a6eccfc867b25aec7ed7b74ca4359a14507112f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-30 13:21:36 +0600

    attempt to fix unnecessary top padding on smaller device

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -4,8 +4,8 @@ import Photo from "./Photo";
 
 const Hero = () => {
   return (
-    <section className="h-screen flex flex-col items-center justify-between pt-0 py-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-background text-foreground">
-      <div className="h-full w-full grid md:grid-cols-3 md:gap-10 gap-4 items-center justify-self-center justify-items-center">
+    <section className="h-screen flex flex-col items-center justify-between pt-0 pb-12 px-6 sm:px-12 md:px-32 lg:px-40 bg-background text-foreground">
+      <div className="h-3/4 md:h-full w-full grid md:grid-cols-3 md:gap-10 items-center justify-self-center justify-items-center">
         <Introduction />
         <Photo />
       </div>
