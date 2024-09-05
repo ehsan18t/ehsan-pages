5de1d53af4d2d4c69fe11ebcc82e02cba135fd4b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-09-05 23:02:00 +0600

    updated socialbar component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgba(var(--primary))",
+        secondary: "rgba(var(--secondary))",
+        background: "rgba(var(--background))",
+        foreground: "rgba(var(--foreground))",
+      },
+      fontFamily: {
+        JetBrainsMono: "var(--font-jettbrainsMono)",
+      },
+    },
+  },
+  plugins: [],
+};
