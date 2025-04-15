43da9ffc22575facf7ed6a0d4b6710c04bec0128
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-04-15 16:14:23 +0600

    fixed typo

--- src/components/sections/footer/contact-terminal/Terminal.jsx
+++ src/components/sections/footer/contact-terminal/Terminal.jsx
@@ -34,7 +34,7 @@ export default function Terminal() {
   const COMMANDS = [
     {
       name: "send",
-      description: "Sends an quick email to Ehsan",
+      description: "Sends a quick email to Ehsan",
       usage: "send <email> <message>",
       handler: async (args) => {
         if (args.length < 2) {
