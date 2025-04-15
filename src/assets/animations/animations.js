81d66def030cbe145f75ef2113425dab2d46e054
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-04-15 19:43:50 +0600

    potential fix for a glitch on mobile device (attempt-2)

--- src/assets/animations/animations.js
+++ src/assets/animations/animations.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+export default function initObservation() {
   const elementsToAnimate = document.querySelectorAll(".animate-when-visible");
 
   const observerOptions = {
@@ -29,4 +29,4 @@ document.addEventListener("DOMContentLoaded", () => {
   elementsToAnimate.forEach((element) => {
     observer.observe(element);
   });
-});
+}
