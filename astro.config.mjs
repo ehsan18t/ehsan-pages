f5b32ee3aa879cff9fa8de5aaf04a169e5aac93f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-04-19 03:46:46 +0600

    added site map and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ehsankhan.me',
   vite: {
     plugins: [tailwindcss()],
   },
   adapter: vercelAdapter(),
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), sitemap()],
 });
