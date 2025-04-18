7a3009e6ed35f85c83c4487a13737b4f27c0520f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-04-19 04:46:22 +0600

    updated some settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ehsankhan.me',
+  site: "https://ehsankhan.me",
   vite: {
     plugins: [tailwindcss()],
   },
