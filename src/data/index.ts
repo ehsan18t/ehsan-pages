288aaf4f6cd81e4f37b484053c05b1a605097864
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-08-28 20:46:51 +0600

    updated projects data

--- src/data/index.ts
+++ src/data/index.ts
@@ -44,6 +44,97 @@ const info = {
       image: "/images/testimonials/fardin.jpg",
     },
   ],
+  projects: [
+    {
+      title: "Resume AI",
+      shortDescription:
+        "An AI based CV screening application designed to simplify the recruitment process for HR professionals and enhance candidate experience. The system integrates Gemini 1.5 Pro for automated profile generation and CV summarization, offering significant improvements in efficiency and decision-making.",
+      imageUrls: [
+        "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1031&q=80",
+      ],
+      techStack: [
+        "NextJS",
+        "Redux",
+        "TailwindCSS",
+        "TypeScript",
+        "Django",
+        "JWT",
+        "PostgreSQL",
+        "REST API",
+      ],
+      repoUrl: "https://github.com/ehsan18t/resume-ai",
+      imageLayout: "cover",
+    },
+    {
+      title: "Cazzert - E-Commerce",
+      shortDescription:
+        "A fully responsive e-commerce site with vercel backend and custom CMS to control everything. Full warehouse managements, order tracking and everything built from scratch.",
+      imageUrls: [
+        "https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1585194166760-6090e40f094e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+      ],
+      techStack: [
+        "Astro",
+        "React",
+        "Server Action",
+        "JWT",
+        "TypeScript",
+        "REST API",
+        "TailwindCSS",
+        "PostgreSQL",
+      ],
+      liveUrl: "https://cazzert.vercel.app/",
+      repoUrl: "https://github.com/ahsan40/cazzert-site",
+      imageLayout: "cover",
+    },
+    {
+      title: "BSIL - Agency",
+      shortDescription:
+        "A fully responsive agency site with vercel backend and custom CMS to control everything. Everything in the site can be managed from admin panel.",
+      imageUrls: [
+        "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1611162617474-5b21e879e113?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80",
+        "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1115&q=80",
+      ],
+      techStack: [
+        "Astro",
+        "React",
+        "Server Action",
+        "JWT",
+        "TypeScript",
+        "REST API",
+        "TailwindCSS",
+        "PostgreSQL",
+      ],
+      liveUrl: "https://bsil-site.vercel.app/",
+      repoUrl: "https://github.com/ahsan40/bsil-site",
+      imageLayout: "cover",
+    },
+    {
+      title: "Course Assistant",
+      shortDescription:
+        "An allrounder helper for undergraduate student which solves real-life problems. It helps to track academic progress, share contents and group study etc. One of the highlight feature is, it can automatically create chat group with students of same section of same trimester.",
+      imageUrls: [
+        "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1031&q=80",
+      ],
+      techStack: [
+        "HTML",
+        "CSS",
+        "TailwindCSS",
+        "JavaScript",
+        "PHP",
+        "Django",
+        "SQL",
+      ],
+      repoUrl: "https://github.com/ehsan18t/CourseAssistant",
+      imageLayout: "cover",
+    },
+  ],
 };
 
 export default info;
