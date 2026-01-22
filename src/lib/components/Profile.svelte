46b413dcaf601b70e6882fba754695fc6346d2cd
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2026-01-22 13:54:58 +0600

    refactor(Profile): convert @apply to pure Tailwind inline classes

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -4,48 +4,36 @@
 	const altText = 'Ehsan Khan';
 </script>
 
-<div class="profile-picture-container">
-	<div class="border-wrapper">
-		<div class="gradient-border animate-when-visible"></div>
-		<img src={meImage} alt={altText} class="profile-picture" />
+<div class="flex select-none items-center justify-center p-5">
+	<div
+		class="relative flex h-[13rem] w-[13rem] items-center justify-center
+			md:h-[16rem] md:w-[16rem] lg:h-[19rem] lg:w-[19rem]"
+	>
+		<!-- Animated gradient border -->
+		<div
+			class="animate-when-visible absolute left-0 top-0 z-10 h-full w-full animate-spin rounded-full p-5"
+			style="
+				animation-duration: 5000ms;
+				background: conic-gradient(
+					from 0deg,
+					rgb(var(--background)),
+					#1c2a71,
+					#1c3871,
+					oklch(var(--accent-900)),
+					oklch(var(--accent-500)),
+					oklch(var(--accent-300))
+				);
+			"
+		></div>
+
+		<!-- Profile image -->
+		<img
+			src={meImage}
+			alt={altText}
+			class="z-20 rounded-full bg-background object-cover
+				h-[calc(13rem-0.313rem)] w-[calc(13rem-0.313rem)]
+				md:h-[calc(16rem-0.4rem)] md:w-[calc(16rem-0.4rem)]
+				lg:h-[calc(19rem-0.438rem)] lg:w-[calc(19rem-0.438rem)]"
+		/>
 	</div>
 </div>
-
-<style>
-	@reference "../../routes/layout.css";
-
-	.profile-picture-container {
-		--sm-pp-size: 13rem; /* 208px */
-		--md-pp-size: 16rem; /* 256px */
-		--lg-pp-size: 19rem; /* 304px */
-
-		--sm-pp-b-size: 0.313rem; /* 5px */
-		--md-pp-b-size: 0.4rem; /* 6px */
-		--lg-pp-b-size: 0.438rem; /* 7px */
-
-		@apply flex select-none items-center justify-center p-5;
-	}
-
-	.border-wrapper {
-		@apply relative flex items-center justify-center;
-		@apply h-(--sm-pp-size) w-(--sm-pp-size) md:h-(--md-pp-size) md:w-(--md-pp-size) lg:h-(--lg-pp-size) lg:w-(--lg-pp-size);
-	}
-
-	.gradient-border {
-		@apply animation-duration-5000 absolute left-0 top-0 z-10 h-full w-full animate-spin rounded-full p-5;
-		background: conic-gradient(
-			from 0deg,
-			rgb(var(--background)),
-			#1c2a71,
-			#1c3871,
-			oklch(var(--accent-900)),
-			oklch(var(--accent-500)),
-			oklch(var(--accent-300))
-		);
-	}
-
-	.profile-picture {
-		@apply z-20 rounded-full bg-background object-cover;
-		@apply h-[calc(var(--sm-pp-size)-var(--sm-pp-b-size))] w-[calc(var(--sm-pp-size)-var(--sm-pp-b-size))] md:h-[calc(var(--md-pp-size)-var(--md-pp-b-size))] md:w-[calc(var(--md-pp-size)-var(--md-pp-b-size))] lg:h-[calc(var(--lg-pp-size)-var(--lg-pp-b-size))] lg:w-[calc(var(--lg-pp-size)-var(--lg-pp-b-size))];
-	}
-</style>
