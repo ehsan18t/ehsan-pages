696145abf1f6d3f6cfee9a0fc9944e281b6c6b97
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2026-02-02 03:01:14 +0600

    feat(hero): redesigned the hero section with tech icons in orbits

--- src/lib/components/hero/index.ts
+++ src/lib/components/hero/index.ts
@@ -3,3 +3,4 @@ export { default as HeroOrbit } from './HeroOrbit.svelte';
 export { default as Profile } from './Profile.svelte';
 export { default as SocialBar } from './SocialBar.svelte';
 export { default as Star } from './Star.svelte';
+export { default as TechIcon } from './TechIcon.svelte';
